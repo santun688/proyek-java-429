e3d26a734602ad2823c11c63d8e0aaa1d2051dc6
santun688 <santundompu379@gmail.com>
2025-10-20 00:21:13 +0700

    up
    mau up

--- Modul 2/tugas/Tugas1/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Doctor doctor = new Doctor();
-        double totalSalary = doctor.applyBonus(2000000);
-        System.out.println("Gaji total setelah bonus: " + totalSalary);
-    }
-}

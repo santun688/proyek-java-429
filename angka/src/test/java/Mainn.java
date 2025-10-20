404d3228ebe66beda9924389682bd5b68a37eaca
santun688 <santundompu379@gmail.com>
2025-10-20 09:16:51 +0700

    update fail
    sya mau update fail baru

---
+++ angka/src/test/java/Mainn.java
@@ -0,0 +1,8 @@
+public class Mainn {
+    public static void main(String[] args) {
+        int r = 5;
+        int e = 2;
+        int kali = r  * e ;
+        System.out.println("total :" + kali );
+    }
+}

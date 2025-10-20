938eec480f26a146b22b504af48fa0eecc6374a9
santun688 <santundompu379@gmail.com>
2025-10-20 09:47:09 +0700

    UPDATE fail
    update fail saya

--- codelab1/src/code.java
+++
@@ -1,9 +0,0 @@
-public class code {
-    public static void main(String[] args) {
-        int b = 1;
-        int v = 2;
-        int kali = b * v ;
-
-        System.out.println("total : " + kali);
-    }
-}
